f64d4be42d2bce44aa64bd56dc09777963c98572
Preet Patel <69676595+preetpatel-github@users.noreply.github.com>
2021-06-15 17:58:10 +0530

    Update S2-Task2.php

--- Session 2/S2-Task2.php
+++ Session 2/S2-Task2.php
@@ -1,14 +1,14 @@
 <!-- Session 2 -> Task-2 -->
 <html>
     <body>
-        <form action="#" method="get" onsubmit="">
+        <form method="POST">
         <label>Please Select Task To Perform:</label>
-            <select value="id">
-                <option id="1"> Varibles </option>
-                <option id="2"> If Condition </option>
-                <option id="3"> leap year </option>
-                <option id="4"> Negative/Positive/Zero Number </option>
-                <option id="5"> Prime Number </option>
+        <select name="TaskSelector">
+                <option selected disabled><-----------------Select--------------></option>
+                <option> Varibles </option>
+                <option> If Condition </option>
+                <option> leap year </option>
+                <option> Negative/Positive/Zero Number </option>
             </select>
         <input id="submit" type="submit" value="Submit" name="Submit"/>
         </form>
@@ -20,11 +20,17 @@
 
 <!-- PHP Switch Logic Code  -->
 <?php
-switch (5) {
-  case 1:
+
+    if(isset($_POST['Submit']))
+    {
+        $gettask=$_POST['TaskSelector'];
+        echo "<b>$gettask :</b> <br><br>";
+    }  
+switch ($gettask) {
+  case "Varibles":
     echo "Variable (With 5 Method)";
-    $a = 10;
-    $b = 20;
+    $a = 5;
+    $b = 10;
     //Method 1
     echo "<br>The Value Of a is ".$a;
     echo "<br>The Value Of b is ".$a;
@@ -37,38 +43,38 @@ switch (5) {
     //Method5
     echo "<br/>Value Of a is $a<br/>Value Of b is $b<br/>Sum is".($a+$b);
     break;
-  case 2:
-      echo "Checkinng age";
+  case "If Condition":
+      echo "Checking age <br>";
       $age=20;
       if ($age==20) {
         echo"Age is 20";
 }
       break;
-  case 3:
+  case "leap year":
       echo "Year is Leap Year or not";
       $my_year = 2022;
-{
+      echo "<br>year is :".$my_year;
     if ($my_year % 400 == 0) {
                 echo"It is a leap year";
             }
         if ($my_year % 4 == 0) {
             echo("It is a leap year");
             } else if ($my_year % 100 == 0) {
-                echo("It is not a leap year");
+                echo("<br>It is not a leap year");
             } else {
-                echo("It is not a leap year");
+                echo("<br>It is not a leap year");
             }
-        }
       break;
-  case 4:
+  case "Negative/Positive/Zero Number":
       echo "Number is eithher +ve,-ve or 0";
-      $x=-10;
-      if ($x > 0)
-    {$message = "Positive number";}
-  if ($x == 0)
-    {$message = "Zero";}
-  if ($x < 0)
-    {$message = "Negative number";}
+      $x= -1;
+      echo "<br>Number is :".$x; 
+      if ($x > 0){
+          $message = "<br>Positive number";}
+  if ($x == 0){
+      $message = "<br>Zero";}
+  if ($x < 0){
+      $message = "<br>Negative number";}
   echo $message."\n";
          break;
     default:
